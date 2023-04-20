8070e51fbb78f761702501c55aa91544b7bfb898
Daniel Omehe <103520487+DanielOmehe@users.noreply.github.com>
2023-04-20 17:43:04 +0000

    Complete project UI

--- main.js
+++ main.js
@@ -1,34 +1,74 @@
-const addExpenseBtn = document.querySelector(".expense");
-const closeBtn = document.querySelector(".close");
-const budgetName = document.querySelector(".name");
-const budgetDescription = document.querySelector(".description");
-const budgetAmount = document.querySelector(".amount");
-const addBudgetForm = document.querySelector(".add-budget-form");
-const chooseCategory = document.querySelector("#choose-category");
+const addExpenseBtn = document.querySelector(".add-expense"),
+addBudgetBtn = document.querySelector(".add-budget"),
+closeBtn = document.querySelector(".close"),
+budgetName = document.querySelector(".expenses-name"),
+budgetDescription = document.querySelector(".expenses-description"),
+budgetAmount = document.querySelector(".expenses-amount"),
+totalBudget = document.querySelector('.total-budget'),
+addBudgetForm = document.querySelector(".add-budget-form"),
+chooseCategory = document.querySelector("#choose-category"),
+amount = document.querySelectorAll('.amount');
 
-addExpenseBtn.addEventListener("click", (e) => {
-  document.querySelector(".add-budget").classList.add("open");
-  document.querySelector(".add-budget-form").classList.add("open");
+console.log(chooseCategory.value);
+
+const string = 'abcdefghijklmnopqrstuvqxyz1234567890';
+
+addExpenseBtn.addEventListener("click", () => {
+  document.querySelector(".add-budget-backdrop").classList.add("open");
+  addBudgetForm.classList.add("open");
 });
 
 closeBtn.addEventListener("click", () => {
-  document.querySelector(".add-budget").classList.remove("open");
-  document.querySelector(".add-budget-form").classList.remove("open");
+  document.querySelector(".add-budget-backdrop").classList.remove("open");
+  addBudgetForm.classList.remove("open");
 });
+
+document.querySelector('.add-budget-backdrop').addEventListener('click', ()=>{
+    document.querySelector('.add-budget-backdrop').classList.remove('open');
+    addBudgetForm.classList.remove('open');
+})
+
+const generateID =(string)=>{
+    let idString = '';
+
+    for(let i = 0; i < string.length / 3 ; i++){
+        idString += string[Math.floor(Math.random() * string.length)]
+    }
+    return idString
+}
+
+const generateCurrency =(amount)=>{
+    const currentcyFormat = new Intl.NumberFormat(undefined, {
+        style: 'currency',
+        currency: 'ngn'
+    })
+
+    const currency = currentcyFormat.format(amount);
+    return currency
+}
+
 
 addBudgetForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const name = budgetName.value;
-  const description = budgetDescription.value;
-  const amount = budgetAmount.value;
+  const name = budgetName.value,
+  description = budgetDescription.value,
+  amount = budgetAmount.value,
+  category = chooseCategory.value
 
   if (name === "" || description === "" || amount === "") {
     console.log("please fill in all fields");
   }
+
+  createNewExpense({
+    id: generateID(string),
+    expense: name,
+    description,
+    amount: generateCurrency(amount),
+    totalBudget: generateCurrency(max)
+  }, category);
 });
 
-document.querySelector(".add-budget").addEventListener("click", () => {
-  document.querySelector(".add-budget").classList.remove("open");
-  document.querySelector(".add-budget-form").classList.remove("open");
-});
+function createNewExpense(expense, category){
+    
+}
