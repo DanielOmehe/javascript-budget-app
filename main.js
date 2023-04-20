a75b5675dcb393e7084fe088d94de56d2517e95d
DaniOmehe <danielomehe66@gmail.com>
2023-04-20 23:19:00 +0100

    write login to add new expense

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 const addExpenseBtn = document.querySelector(".add-expense"),
-addBudgetBtn = document.querySelector(".add-budget"),
-closeBtn = document.querySelector(".close"),
-budgetName = document.querySelector(".expenses-name"),
-budgetDescription = document.querySelector(".expenses-description"),
-budgetAmount = document.querySelector(".expenses-amount"),
-totalBudget = document.querySelector('.total-budget'),
-addBudgetForm = document.querySelector(".add-budget-form"),
-chooseCategory = document.querySelector("#choose-category"),
-expensesTable = document.querySelector('.budget-account-table'),
-amount = document.querySelectorAll('.amount');
+  addBudgetBtn = document.querySelector(".add-budget"),
+  closeBtn = document.querySelector(".close"),
+  budgetName = document.querySelector(".expenses-name"),
+  budgetDescription = document.querySelector(".expenses-description"),
+  budgetAmount = document.querySelector(".expenses-amount"),
+  totalBudget = document.querySelector(".total-budget"),
+  addBudgetForm = document.querySelector(".add-budget-form"),
+  chooseCategory = document.querySelector("#choose-category"),
+  expensesTable = document.querySelector(".budget-account-table"),
+  amount = document.querySelectorAll(".amount"),
+  maxBudgetAmount = document.querySelectorAll(".max");
 
-console.log(chooseCategory.value);
+let expenses = [],
+  max = 0;
 
-const string = 'abcdefghijklmnopqrstuvqxyz1234567890';
+window.addEventListener("load", () => {
+  amount.forEach((amount) => {
+    amount.innerHTML = generateCurrency(0);
+  });
+  maxBudgetAmount.forEach((amount) => {
+    amount.innerHTML = generateCurrency(0);
+  });
+});
+const string = "abcdefghijklmnopqrstuvqxyz1234567890";
 
 addExpenseBtn.addEventListener("click", () => {
   document.querySelector(".add-budget-backdrop").classList.add("open");
@@ -24,52 +34,82 @@ closeBtn.addEventListener("click", () => {
   addBudgetForm.classList.remove("open");
 });
 
-document.querySelector('.add-budget-backdrop').addEventListener('click', ()=>{
-    document.querySelector('.add-budget-backdrop').classList.remove('open');
-    addBudgetForm.classList.remove('open');
-})
+document.querySelector(".add-budget-backdrop").addEventListener("click", () => {
+  document.querySelector(".add-budget-backdrop").classList.remove("open");
+  addBudgetForm.classList.remove("open");
+});
 
-const generateID =(string)=>{
-    let idString = '';
+const generateID = (string) => {
+  let idString = "";
 
-    for(let i = 0; i < string.length / 3 ; i++){
-        idString += string[Math.floor(Math.random() * string.length)]
-    }
-    return idString
-}
+  for (let i = 0; i < string.length / 3; i++) {
+    idString += string[Math.floor(Math.random() * string.length)];
+  }
+  return idString;
+};
 
-const generateCurrency =(amount)=>{
-    const currentcyFormat = new Intl.NumberFormat(undefined, {
-        style: 'currency',
-        currency: 'ngn'
-    })
+const generateCurrency = (amount) => {
+  const currentcyFormat = new Intl.NumberFormat(undefined, {
+    style: "currency",
+    currency: "ngn",
+  });
 
-    const currency = currentcyFormat.format(amount);
-    return currency
-}
-
+  const currency = currentcyFormat.format(amount);
+  return currency;
+};
 
 addBudgetForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const name = budgetName.value,
-  description = budgetDescription.value,
-  amount = budgetAmount.value,
-  category = chooseCategory.value
+    description = budgetDescription.value,
+    amount = budgetAmount.value,
+    category = chooseCategory.value,
+    max = totalBudget.value;
 
   if (name === "" || description === "" || amount === "") {
-    console.log("please fill in all fields");
+    alert("please fill in all fields");
+    e.stopPropagation();
+  } else {
+    createNewExpense(
+      {
+        id: generateID(string),
+        name: name,
+        description,
+        amount: generateCurrency(amount),
+        category,
+        totalBudget: generateCurrency(max),
+      },
+      category
+    );
   }
 
-  createNewExpense({
-    id: generateID(string),
-    expense: name,
-    description,
-    amount: generateCurrency(amount),
-    totalBudget: generateCurrency(max)
-  }, category);
+  budgetName.value = "";
+  budgetDescription.value = "";
+  budgetAmount.value = "";
+  totalBudget.value = "";
 });
 
-function createNewExpense(expense, category){
-  
+function createNewExpense(expense, category) {
+  expenses.push(expense);
+  expenses.map((expense, index) => {
+    expensesTable.innerHTML += `
+      <tr class="table-row" id='${expense.id}'>
+        <td>${index + 1}</td>
+        <td>${expense.name}</td>
+        <td>${expense.description}</td>
+        <td>${expense.amount}</td>
+        <td>${expense.category}</td>
+        <td>
+          <button class="button">
+            <i class='fa-solid fa-pen'></i>
+          </button>
+        </td>
+        <td>
+          <button class="button">
+            <i class='fa-solid fa-trash'></i>
+          </button>
+        </td>
+</tr>`;
+  });
 }
